b3367b57affbe67750f1dcbf16f366083604fff4
mukhriddin-dev <khodievm@gmail.com>
2022-09-27 20:58:11 +0500

    ok

---
+++ module-3/lesson-3/script.js
@@ -0,0 +1,156 @@
+"use strict";
+
+// let dayNumber = prompt('enter value')
+
+// if (dayNumber === 'green') {
+
+//    console.log('GO');
+
+// } else if (dayNumber === 'red') {
+
+//    console.log('STOP');
+
+// } else if (dayNumber === 'yellow') {
+
+//    console.log('ATTENTION!');
+
+// } else {
+
+//    console.log('404');
+
+// }
+
+// 
+
+
+
+
+// if (dayNumber >= 1 && dayNumber < 4 && dayNumber>11) {
+
+//    console.log('Dushanba');
+//    console.log('Seshanba');
+//    console.log('chorshanba');
+
+// } else if (dayNumber >= 4 && dayNumber<7) {
+
+//    console.log('payshanba');
+//    console.log('Juma');
+//    console.log('shanba');
+
+
+// } else if (dayNumber === 7) {
+//    console.log('yakshanba');
+// } else {
+
+//    console.log('404');
+
+// }
+
+
+
+
+// switch (dayNumber) {
+//    case 1:
+//       console.log('Dushnba');
+//       break;
+//    case 2:
+//       console.log('Seshanba');
+//       break;
+//    case 3:
+//       console.log('Chorshanba');
+//       break;
+//    case 4:
+//       console.log('Payshanba');
+//       break;
+//    case 5:
+//       console.log('Juma');
+//       break;
+//    case 6:
+//       console.log('Shanba');
+//       break;
+//    case 7:
+//       console.log('Yakshanba');
+//       break;
+//       default : console.log('BUNDAY KUN YO\'Q');
+
+// }
+
+// let dayNumber = +prompt('enter value');
+
+// if (dayNumber % 2 === 1) {
+//    console.log(`Bu son ${dayNumber} toq`);
+// } else {
+//    console.log(`Bu son ${dayNumber} toq emas`);
+// }
+
+//5-m
+// let a=+prompt('a=');
+// let b=+prompt('b=');
+// let c=+prompt('c=');
+
+
+// if(a>b && b>c){
+// console.log('xa');
+// }else{
+//    alert('yo\'q')
+// }
+
+// (3<2) ? console.log('bu tog\'ri') : console.log(' bu xato');
+
+// loop , while , do while , for 
+
+// console.log('javaScript');
+// console.log('javaScript');
+// console.log('javaScript');
+// console.log('javaScript');
+// console.log('javaScript');
+// console.log('javaScript');
+// console.log('javaScript');
+// console.log('javaScript');
+
+
+// let n = 1000; // initial state
+
+// while (n > 0) {
+
+//    n--
+
+//    if (n % 2 == 1) {
+//       console.log(n);
+//    }
+
+// }
+
+// do {
+//    n--;
+//    console.log(n);
+// } while (n > 0)
+
+// let n = +prompt('n')
+
+// for (let i = 0; i <= n; i+=2) {
+
+//    console.log(i);
+   // if (i % 2 == 1) {
+   //    console.log(i);
+   // }
+
+// }
+
+
+
+// let kg=1;
+
+// for (let i=1; i<=10; i++){
+//    console.log(`${i/10} kg narxi ${(i/10)*10000} so'm`);
+// }
+
+
+for(let i=1; i<=100; i++){
+
+   for(let e=1; e<=100; e++){
+         document.write(`${i} x ${e} = ${i*e} <br>`);
+         console.log(e);
+   }
+
+}
