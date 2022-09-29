efd48bf2a6630f47c9113d09493f400ce486c90f
mukhriddin-dev <khodievm@gmail.com>
2022-09-29 18:21:15 +0500

    ok

---
+++ module-3/lesson-1/script.js
@@ -0,0 +1,47 @@
+"use strict"
+
+// javaScriptda 8 ta data type bor
+
+// String => "JavaScript" , 'nodejs' , `personal home page => php`
+// "122" , '8888' , `122222` , 'true';
+
+
+
+// Number => 12, 0.12 ,  , 1 , 0 , 11111111111111
+
+
+
+// Boolean => true =1 , false=0
+
+
+// o'zgaruvchi bor qiymati topilmasa => Undefined
+
+// o'zgaruvchi bor qiymat bo'sh => null
+
+
+// console.log(typeof(true));
+
+
+// var ism=null;
+
+// console.log(ism);
+
+/// ES5 , ES6
+
+// ('666') ? console.log('1') : console.log('Notification');
+
+
+// m-1
+
+// let sm=+prompt('sm kiriting');
+// console.log(sm/100+" metr");
+
+// m-2
+
+
+
+
+
+        
+
+
---
+++ module-3/lesson-2/script.js
@@ -0,0 +1,122 @@
+"use strict";
+
+// document.write('JavaScript');
+
+// console.log('javaScript');
+// console.log(window);
+
+// alert('Salom') 
+// prompt('ismingiz nima ?')
+// confirm('saytdan chiqasizmi ?')
+
+// ===== VAR
+// console.log(a);
+// {
+//    var a = false;
+//    var a = 66;
+// }
+
+// console.log(a);
+
+// ===== LET
+// ES5
+
+// {
+//    let myName = 'Mukhriddin';
+//    myName = 'Husniddin';
+//    console.log(myName); 
+// }
+
+// let myName = 'Mukhriddin';
+// console.log(myName); 
+
+
+// ES6
+
+// {
+//    const app = 'telegram';
+//    console.log(app);
+// }
+
+// const app='facebook';
+
+// console.log(app);
+
+// app='whatsapp';
+// const app='Lite FB';
+
+// console.log(app);
+
+
+// 2. 
+
+//  + , - ,  / , * , ** , % , ++  INCREMENTR , -- DICREMENT
+
+
+// let num=10;
+
+// num--
+
+// console.log(num);
+// console.log(num);
+
+
+// console.log(num);
+
+
+// console.log(num%13);
+
+// let a=2022;
+// let b=2023;
+
+// a=a+b;  
+// a=a-b;
+// a=a*b;
+// a=a/b;
+// a=a**b;
+// a=a%b;
+
+// a+=b;
+// a-=b;
+// a**=b;
+// a%=b;
+
+// let str='javaScript';
+// str+="lorem ipsum dolor set dsfgrdthrydvrge we5tw4re4tfeq";
+// let str2="app";
+// str2+=str;
+// console.log(str2);
+
+
+// console.log(str);
+
+// let num=12.4912343654765865;
+
+// console.log(Math.ceil(num)); katta tomonga
+// console.log(Math.floor(-122.1 -123)); 
+// console.log(Math.round(num)); haqiqiy
+// console.log(Math.random()); // tasodifiyraqamlar (0, 1)
+// console.log(Math.pow(25,1/));
+// console.log(Math.sqrt(49));
+// console.log(Math.abs(-222));
+// console.log(Math.max(2,-33,11,99));
+// console.log(Math.trunc(12.99)); butun qismni olib beradi
+
+// console.log(num.toFixed());
+
+
+let a=12;
+let b=3;
+
+
+console.log(a<`b);
+
+
+
+
+
+
+
+
+
+
