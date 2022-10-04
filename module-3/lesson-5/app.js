46a51f755209fbea407925c056c6f8fde7fadb84
mukhriddin-dev <khodievm@gmail.com>
2022-10-04 21:06:21 +0500

    lesson 5 completed

---
+++ module-3/lesson-5/app.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let div=document.getElementsByTagName('h1'); // hamma elemetlarni array qilib qaytaratdi
+
+let elements=document.getElementsByClassName('col-6'); //
+// hamma elemetlarni array qilib qaytaratdi
+
+
+// bitta elementni qaytaradi
+
+let success=document.getElementById('bg-s');
+
+// console.log(success);
+
+// all / and   
+let el=document.querySelector('.ok');
+
+console.log(el);
+
+
+
+// console.log(elements);
+
+// elements[0].style.display="none";
+
+
+// div[0].style.color="red";
+// console.log(div);
+
+
+
+// const style={
+//    color:'red',
+//    color:'green'
+// }
+
+// console.log(style);
