899feb8354125004f532277ce3d8a5701bc5d552
mukhriddin-dev <khodievm@gmail.com>
2022-10-04 20:38:54 +0500

    ok

--- module-3/lesson-5/script.js
+++ module-3/lesson-5/script.js
@@ -1,1 +1,90 @@
 "use strict";
+
+// const obj=new Object({
+//    isname:'BU OBYEKT'
+// })
+
+
+// console.log(obj);
+
+// console.log(window);
+// let person = ['ism', 'yosh'];
+
+// console.log(person[0]);
+
+// let javlonbek = {
+
+//    isname: "Javlonbek",
+//    age: 16,
+//    hobbies: ['coding', 'read book', 'play PUBG', ['math', 'physic', {
+//       english:"ESSENTIAL 1"
+//    }]],
+//    job: 'programmer',
+
+//    laptop: {
+//       brand: 'HP',
+//       model: 'pavillon Gaming',
+//       color: 'balck'
+//    }
+
+// }
+
+// console.log(Object.values(javlonbek));
+
+
+
+// console.log(javlonbek['hobbies'][3][2].english);
+
+// console.log(javlonbek['isname']);
+
+// console.log(javlonbek['laptop']['brand']);
+
+
+// console.log(javlonbek.hobbies[3][2]);
+
+// console.log(javlonbek.isname);
+
+// console.log(javlonbek.laptop.model);
+
+
+// const room={
+//    isname:'Amazon',
+//    size:25,
+//    color:['yellow','grey','white','black'],
+//    type:'medium',
+//    fullInfo: function(e){
+//       console.log(`bu xabar ${e}`);
+//    }
+// }
+
+// for(let key in room){
+//    // console.log(key);
+//    console.log(key+": "+room[key]);
+//    // console.log(room[key]);
+// }
+
+// console.log(room['type']);
+
+// console.log(Object.keys(room));
+// console.log(Object.values(room));
+// console.log(Object.entries(room));
+
+// const obj=Object.entries(room);
+
+// console.log(Object.fromEntries(obj));
+
+
+
+// const array=[1,2,3,4];
+
+// console.log(array.push());
+
+
+// window.alert('salom')
+
+// room.fullInfo('salom')
+
+// room.calc(12,"+",11);
+
+
+
