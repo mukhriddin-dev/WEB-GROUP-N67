5f3cc6f7838298a26e0e518b22f41460dc32e28f
mukhriddin-dev <khodievm@gmail.com>
2022-10-18 21:08:19 +0500

    lesson 11 completed

--- module-3/lesson-11/script.js
+++ module-3/lesson-11/script.js
@@ -1,2 +1,32 @@
 "use strict";
 
+movies.splice(50);
+
+// ============= NORMLIZE MOVIES ==========////
+
+const allMovies = movies.map((e) => {
+  return {
+    title: e.title,
+    year: e.year,
+    category: e.categories,
+    id: e.imdbId,
+    rating: e.imdbRating,
+    time: `${Math.trunc(e.runtime / 60)} soat ${e.runtime % 60} daqiqa`,
+    lang: e.language,
+    yotube: `https://www.youtube.com/embed/${e.youtubeId}`,
+    summary: e.summary,
+    smallImg: e.smallThumbnail,
+    largeImg: e.bigThumbnail,
+  };
+});
+
+console.log(allMovies);
+
+// ============= NORMLIZE MOVIES END ==========////
+// EXTRA PLUGINS//
+
+(function () {
+  const date = new Date();
+  const time = `${date.getFullYear()}`;
+  $("#copyright").innerHTML = time;
+})();
